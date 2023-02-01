dd06b209ffeef2270587cf227deb96afa5d05bbe
Andres Hernandez Serna <mauricioserna123@gmail.com>
2023-01-31 22:54:53 -0500

    update features

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent {
+  editTodo: boolean =false;
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
+
+  updateToggle() {
+    this.editTodo = !this.editTodo;
+  }
+
+}
